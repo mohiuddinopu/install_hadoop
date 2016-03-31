a5755c204787f926f9be8a8bd3d9198950d2ce1c
mohiuddinopu <mohi.hadoop.ad@gmail.com>
2016-03-30 23:10:19 -0400

    Update bashrc.sh

--- install_hadoop/bashrc.sh
+++ install_hadoop/bashrc.sh
@@ -1,9 +1,9 @@
 export HADOOP_HOME=/opt/hadoop
-export HADOOP_INSTALL=$HADOOP_HOME
-export HADOOP_MAPRED_HOME=$HADOOP_HOME
-export HADOOP_COMMON_HOME=$HADOOP_HOME
-export HADOOP_HDFS_HOME=$HADOOP_HOME
-export YARN_HOME=$HADOOP_HOME
-export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
-export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
+export HADOOP_INSTALL=/opt/hadoop
+export HADOOP_MAPRED_HOME=/opt/hadoop
+export HADOOP_COMMON_HOME=/opt/hadoop
+export HADOOP_HDFS_HOME=/opt/hadoop
+export YARN_HOME=/opt/hadoop
+export HADOOP_COMMON_LIB_NATIVE_DIR=/opt/hadoop/lib/native
+export PATH=$PATH:/opt/hadoop/sbin:/opt/hadoop/bin
 export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk.x86_64/
